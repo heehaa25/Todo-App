71e41fb3e604a069947fa86eb6f1a68f1ee4f8c7
heehaa25 <black1berrychoi@gmail.com>
2025-07-02 13:58:20 +0900

    수정 버튼 클릭 시에만 input 보이도록 수정 그 외 CSS 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 0, content: "123" },
-    { id: 1, content: "코딩 공부하기" },
-    { id: 2, content: "잠 자기" },
+    { id: 0, content: '123' },
+    { id: 1, content: '코딩 공부하기' },
+    { id: 2, content: '잠 자기' },
   ]);
 
   return (
-    <>
+    <div className='container'>
+      <h1>Todo List</h1>
       <TodoList todoList={todoList} setTodoList={setTodoList} />
       <hr />
       <TodoInput todoList={todoList} setTodoList={setTodoList} />
-    </>
+    </div>
   );
 }
 
 function TodoInput({ todoList, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   return (
-    <>
+    <div className='input__container'>
       <input
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
       />
       <button
+        className='add__btn'
         onClick={() => {
           const newTodo = { id: Number(new Date()), content: inputValue };
           const newTodoList = [...todoList, newTodo];
           setTodoList(newTodoList);
-          setInputValue("");
+          setInputValue('');
         }}
       >
         추가하기
       </button>
-    </>
+    </div>
   );
 }
 
 function TodoList({ todoList, setTodoList }) {
   return (
-    <ul>
+    <ul className='todo__container'>
       {todoList.map((todo) => (
         <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
       ))}
@@ -51,34 +53,46 @@ function TodoList({ todoList, setTodoList }) {
 }
 
 function Todo({ todo, setTodoList }) {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
-    <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
+    <li className='todo__list'>
+      <div className='todo'>
+        <input type='checkbox' id='checkbox' />
+        <span>{todo.content}</span>
+        {isEditing && (
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+        )}
+      </div>
+      <div className='todo__bts'>
+        <button
+          className='isEditing__btn'
+          onClick={() => {
+            setTodoList((prev) =>
+              prev.map((el) =>
+                el.id === todo.id ? { ...el, content: inputValue } : el
+              )
+            );
+            setIsEditing(!isEditing);
+          }}
+        >
+          {isEditing ? '저장' : '수정'}
+        </button>
+        <button
+          className='delete__btn'
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}
+        >
+          삭제
+        </button>
+      </div>
     </li>
   );
 }
